e0dad87574745ed89f6a512fc5083a3874015935
ChronoVortex <nick@hudsucker.com>
2023-08-07 09:37:15 -0700

    initial commit

---
+++ data/escort_scripts/escort-duty-ach.lua
@@ -0,0 +1,78 @@
+----------------------
+-- HELPER FUNCTIONS --
+----------------------
+
+local vter = mods.vertexutil.vter
+local userdata_table = mods.vertexutil.userdata_table
+
+local function string_starts(str, start)
+    return string.sub(str, 1, string.len(start)) == start
+end
+
+local function should_track_achievement(achievement, ship, shipClassName)
+    return ship and
+           Hyperspace.Global.GetInstance():GetCApp().world.bStartedGame and
+           Hyperspace.CustomAchievementTracker.instance:GetAchievementStatus(achievement) < Hyperspace.Settings.difficulty and
+           string_starts(ship.myBlueprint.blueprintName, shipClassName)
+end
+
+-- Track changes in player ship hull value
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
+    if ship.iShipId == 0 and string_starts(ship.myBlueprint.blueprintName, "PLAYER_SHIP_ESCORT_DUTY") then
+        local shipData = userdata_table(ship, "mods.escort.achTracking")
+        local hullCurrent = ship.ship.hullIntegrity.first
+        shipData.hullChange = hullCurrent - (shipData.hullLast or hullCurrent)
+        shipData.hullLast = hullCurrent
+    end
+end)
+
+-------------------------
+-- ACHIEVEMENT UNLOCKS --
+-------------------------
+
+-- Easy
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
+    if ship.iShipId == 0 and should_track_achievement("ACH_SHIP_ESCORT_DUTY_1", ship, "PLAYER_SHIP_ESCORT_DUTY") then
+        local shipData = userdata_table(ship, "mods.escort.achTracking")
+        local enemyShip = Hyperspace.ships.enemy
+        if enemyShip and enemyShip._targetable.hostile and not enemyShip.bDestroyed then
+            if shipData.hullChange > 0 then
+                shipData.hullRepairedThisFight = shipData.hullRepairedThisFight + shipData.hullChange
+                if shipData.hullRepairedThisFight >= 20 then
+                    Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_ESCORT_DUTY_1", false)
+                    return
+                end
+            end
+        else
+            shipData.hullRepairedThisFight = 0
+        end
+    end
+end)
+
+-- Normal
+script.on_internal_event(Defines.InternalEvents.DRONE_FIRE, function(projectile, drone)
+    if drone.iShipId == 0 and drone.blueprint.targetType == 4 and drone.blueprint.typeName == "DEFENSE" and should_track_achievement("ACH_SHIP_ESCORT_DUTY_2", Hyperspace.ships.player, "PLAYER_SHIP_ESCORT_DUTY") then
+        userdata_table(projectile, "mods.escort.achTracking").firedFromProjDrone = true
+    end
+end)
+script.on_internal_event(Defines.InternalEvents.DRONE_COLLISION, function(drone, projectile, damage, response)
+    if response.collision_type == 1 and drone.iShipId == 1 and drone.blueprint.typeName == "COMBAT" and should_track_achievement("ACH_SHIP_ESCORT_DUTY_2", Hyperspace.ships.player, "PLAYER_SHIP_ESCORT_DUTY") and userdata_table(projectile, "mods.escort.achTracking").firedFromProjDrone then
+        Hyperspace.playerVariables.loc_escort_drones_destroyed = Hyperspace.playerVariables.loc_escort_drones_destroyed + 1
+        if Hyperspace.playerVariables.loc_escort_drones_destroyed >= 3 then
+            Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_ESCORT_DUTY_2", false)
+        end
+    end
+end)
+
+-- Hard
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(ship)
+    if ship.iShipId == 0 and Hyperspace.playerVariables.loc_escort_damage_taken <= 5 and should_track_achievement("ACH_SHIP_ESCORT_DUTY_3", ship, "PLAYER_SHIP_ESCORT_DUTY") then
+        local hullChange = userdata_table(ship, "mods.escort.achTracking").hullChange
+        if hullChange < 0 then
+            Hyperspace.playerVariables.loc_escort_damage_taken = Hyperspace.playerVariables.loc_escort_damage_taken - hullChange
+        end
+        if Hyperspace.playerVariables.loc_sector_count > 3 then
+            Hyperspace.CustomAchievementTracker.instance:SetAchievement("ACH_SHIP_ESCORT_DUTY_3", false)
+        end
+    end
+end)
---
+++ data/escort_scripts/escort-duty.lua
@@ -0,0 +1,123 @@
+if not mods or not mods.vertexutil then
+    error("Couldn't find Vertex Tags and Utility Functions! Make sure it's above mods which depend on it in the Slipstream load order", 2)
+end
+
+local vter = mods.vertexutil.vter
+local get_ship_crew_point = mods.vertexutil.get_ship_crew_point
+local ShowTutorialArrow = mods.vertexutil.ShowTutorialArrow
+local HideTutorialArrow = mods.vertexutil.HideTutorialArrow
+
+------------------------
+-- CUSTOM DRONE ORBIT --
+------------------------
+local escortEllipse = {
+    center = {
+        x = 139,
+        y = 280
+    }, 
+    a = 229,
+    b = 103
+}
+local droneSpeedFactor = 1.6
+local activeDroneIds = {}
+
+local calculate_coord_offset = function(angleFromCenter)
+    local angleCos = escortEllipse.b*math.cos(angleFromCenter)
+    local angleSin = escortEllipse.a*math.sin(angleFromCenter)
+    local denom = math.sqrt(angleCos^2 + angleSin^2)
+    return (escortEllipse.a*angleCos)/denom, (escortEllipse.b*angleSin)/denom
+end
+
+script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
+    -- Iterate through all defense drones if the ship is escort duty
+    local isEscort = 0
+    if pcall(function() isEscort = Hyperspace.ships.player:HasEquipment("SHIP PLAYER_SHIP_ESCORT_DUTY") end) and isEscort > 0 then
+        local stillActive = {}
+        for drone in vter(Hyperspace.ships.player.spaceDrones) do
+            if drone.deployed and drone.blueprint.typeName == "DEFENSE" then
+                stillActive[drone.selfId] = true
+                local xOffset = nil
+                local yOffset = nil
+                
+                -- Set drone location to a random point on the ellipse if just deployed
+                if not activeDroneIds[drone.selfId] then
+                    activeDroneIds[drone.selfId] = true
+                    xOffset, yOffset = calculate_coord_offset((Hyperspace.random32()%360)*(math.pi/180))
+                    drone:SetCurrentLocation(Hyperspace.Pointf(
+                        escortEllipse.center.x + xOffset,
+                        escortEllipse.center.y + yOffset))
+                end
+                
+                -- Make the drone orbit the fake ellipse
+                if drone.powered then
+                    local lookAhead = drone.blueprint.speed*Hyperspace.FPS.SpeedFactor/droneSpeedFactor
+                    xOffset, yOffset = calculate_coord_offset(math.atan(
+                        drone.currentLocation.y - escortEllipse.center.y,
+                        drone.currentLocation.x - escortEllipse.center.x))
+                    local xIntersect = escortEllipse.center.x + xOffset
+                    local yIntersect = escortEllipse.center.y + yOffset
+                    local tanAngle = math.atan((escortEllipse.b^2/escortEllipse.a^2)*(xOffset/yOffset))
+                    if (drone.currentLocation.y < escortEllipse.center.y) then
+                        drone.destinationLocation = Hyperspace.Pointf(
+                            xIntersect + lookAhead*math.cos(tanAngle),
+                            yIntersect - lookAhead*math.sin(tanAngle))
+                    else
+                        drone.destinationLocation = Hyperspace.Pointf(
+                            xIntersect - lookAhead*math.cos(tanAngle),
+                            yIntersect + lookAhead*math.sin(tanAngle))
+                    end
+                end
+            end
+        end
+        
+        -- Clean out inactive drone IDs
+        for droneId in pairs(activeDroneIds) do
+            if not stillActive[droneId] then
+                activeDroneIds[droneId] = nil
+            end
+        end
+    end
+end)
+
+-----------------------------
+-- SHORT-RANGE TRANSPORTER --
+-----------------------------
+script.on_game_event("INITIAL_START_BEACON_ESCORT_DUTY", false, function()
+    ShowTutorialArrow(1, 918, 561)
+end)
+script.on_game_event("LUA_HIDE_ESCORT_TUTORIAL_ARROW", false, HideTutorialArrow)
+script.on_game_event("START_BEACON_PRESELECT", false, HideTutorialArrow) -- Just in case the tut arrow is active on game start
+
+-- Coordinates of the individual TP pads on the escort fighter
+local tpPadsShip1 = {
+    { x = 350, y = 140 },
+    { x = 385, y = 140 }
+}
+-- Coordinates of the individual TP pads on the freighter
+local tpPadsShip2 = {
+    { x = 210, y = 270 },
+    { x = 245, y = 270 },
+}
+local padCount = 2
+script.on_game_event("LUA_ESCORT_TELEPORT", false, function()
+    local playTpSound = false
+    for padIndex = 1, padCount, 1 do
+        -- Check for crew on the pad of the first ship and teleport
+        local crewShip1 = get_ship_crew_point(Hyperspace.ships.player,
+            tpPadsShip1[padIndex].x, tpPadsShip1[padIndex].y, 1)[1]
+        if crewShip1 and not crewShip1:IsDrone() then -- No drones 'cause no TP anims
+            Hyperspace.Get_CrewMember_Extend(crewShip1):InitiateTeleport(0, 6)
+            playTpSound = true
+        end
+        -- Check for crew on the pad of the second ship and teleport
+        local crewShip2 = get_ship_crew_point(Hyperspace.ships.player,
+            tpPadsShip2[padIndex].x, tpPadsShip2[padIndex].y, 1)[1]
+        if crewShip2 and not crewShip2:IsDrone() then
+            Hyperspace.Get_CrewMember_Extend(crewShip2):InitiateTeleport(0, 5)
+            playTpSound = true
+        end
+    end
+    if playTpSound then
+        Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("teleport", 1, false)
+    end
+end)
