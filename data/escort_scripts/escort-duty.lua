9f34f83446a85176f76613dd3d066af2783d62d3
ChronoVortex <33610911+ChronoVortex@users.noreply.github.com>
2024-06-24 07:41:37 -0700

    modularize local teleporter code

--- data/escort_scripts/escort-duty.lua
+++ data/escort_scripts/escort-duty.lua
@@ -21,7 +21,21 @@ local escortDutyShips = {
             a = 229,
             b = 103
         },
-        droneSpeedFactor = 1.6
+        droneSpeedFactor = 1.6,
+        tpLinkages = {
+            -- Coordinates of the individual TP pads on the escort fighter
+            tpRoomShip1 = 5,
+            tpPadsShip1 = {
+                { x = 350, y = 140, slot = 0 },
+                { x = 385, y = 140, slot = 1 }
+            },
+            -- Coordinates of the individual TP pads on the freighter
+            tpRoomShip2 = 6,
+            tpPadsShip2 = {
+                { x = 210, y = 270, slot = 0 },
+                { x = 245, y = 270, slot = 1 },
+            }
+        }
     }
 }
 
@@ -96,42 +110,39 @@ end)
 -----------------------------
 -- SHORT-RANGE TRANSPORTER --
 -----------------------------
-script.on_game_event("INITIAL_START_BEACON_ESCORT_DUTY", false, function()
-    ShowTutorialArrow(1, 918, 561)
-end)
+script.on_game_event("INITIAL_START_BEACON_ESCORT_DUTY", false, function() ShowTutorialArrow(1, 918, 561) end)
 script.on_game_event("LIGHTSPEED_DROPPOINT", false, HideTutorialArrow)
 script.on_game_event("START_BEACON_PRESELECT", false, HideTutorialArrow) -- Just in case the tut arrow is active on game start
 
--- Coordinates of the individual TP pads on the escort fighter
-local tpPadsShip1 = {
-    { x = 350, y = 140 },
-    { x = 385, y = 140 }
-}
--- Coordinates of the individual TP pads on the freighter
-local tpPadsShip2 = {
-    { x = 210, y = 270 },
-    { x = 245, y = 270 },
-}
-local padCount = 2
 script.on_game_event("LUA_ESCORT_TELEPORT", false, function()
-    local playTpSound = false
-    for padIndex = 1, padCount, 1 do
-        -- Check for crew on the pad of the first ship and teleport
-        local crewShip1 = get_ship_crew_point(Hyperspace.ships.player,
-            tpPadsShip1[padIndex].x, tpPadsShip1[padIndex].y, 1)[1]
-        if crewShip1 and not crewShip1:IsDrone() then -- No drones 'cause no TP anims
-            Hyperspace.Get_CrewMember_Extend(crewShip1):InitiateTeleport(0, 6)
-            playTpSound = true
+    local ship = Hyperspace.ships.player
+    if ship then
+        for shipName, escortData in pairs(escortDutyShips) do
+            -- Check if ship is an escort duty variant
+            if shipName == ship.myBlueprint.blueprintName then
+                local playTpSound = false
+                local links = escortData.tpLinkages
+                for padIndex = 1, #(links.tpPadsShip1), 1 do
+                    -- Check for crew on the pad of the first ship and teleport
+                    local crewShip1 = get_ship_crew_point(ship,
+                        links.tpPadsShip1[padIndex].x, links.tpPadsShip1[padIndex].y, 1)[1]
+                    if crewShip1 and not crewShip1:IsDrone() then -- No drones 'cause no TP anims
+                        crewShip1.extend:InitiateTeleport(0, links.tpRoomShip2, links.tpPadsShip2[padIndex].slot)
+                        playTpSound = true
+                    end
+                    -- Check for crew on the pad of the second ship and teleport
+                    local crewShip2 = get_ship_crew_point(ship,
+                        links.tpPadsShip2[padIndex].x, links.tpPadsShip2[padIndex].y, 1)[1]
+                    if crewShip2 and not crewShip2:IsDrone() then
+                        crewShip2.extend:InitiateTeleport(0, links.tpRoomShip1, links.tpPadsShip1[padIndex].slot)
+                        playTpSound = true
+                    end
+                end
+                if playTpSound then
+                    Hyperspace.Sounds:PlaySoundMix("teleport", -1, false)
+                end
+                break
+            end
         end
-        -- Check for crew on the pad of the second ship and teleport
-        local crewShip2 = get_ship_crew_point(Hyperspace.ships.player,
-            tpPadsShip2[padIndex].x, tpPadsShip2[padIndex].y, 1)[1]
-        if crewShip2 and not crewShip2:IsDrone() then
-            Hyperspace.Get_CrewMember_Extend(crewShip2):InitiateTeleport(0, 5)
-            playTpSound = true
-        end
-    end
-    if playTpSound then
-        Hyperspace.Global.GetInstance():GetSoundControl():PlaySoundMix("teleport", 1, false)
     end
 end)
